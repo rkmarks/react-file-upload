cf74bbb416d9cea820d921487518f83d235a689a
rkmarks <rmarkins@gmail.com>
2017-08-23 19:14:45 -0700

    initial commit

---
+++ app/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    constructor() {
+        super(...arguments);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>My Small Change</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
